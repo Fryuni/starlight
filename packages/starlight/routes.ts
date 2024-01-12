cf82172bab7c7b0c0e0c67332d00be4ae420f8fa
Luiz Ferraz <luiz@lferraz.com>
2024-01-11 22:41:35 -0300

    Remove export of `generateRouteData`

--- packages/starlight/routes.ts
+++ packages/starlight/routes.ts
@@ -1,3 +1,2 @@
 export { routes, paths, getRouteBySlugParam, getLocaleRoutes, type Route } from './utils/routing';
-export { generateRouteData } from './utils/route-data';
 export { slugToPathname, slugToParam } from './utils/slugs';
